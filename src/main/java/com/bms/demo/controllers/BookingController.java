a867364c89930662b88f75e5655e71849b2c4347
RavitejaBaira <ravitejabaira@gmail.com>
2024-02-10 15:47:50 +0530

    Removed something which was not essential

--- src/main/java/com/bms/demo/controllers/BookingController.java
+++ src/main/java/com/bms/demo/controllers/BookingController.java
@@ -28,8 +28,6 @@ public class BookingController {
             Booking booking = bookingService.bookShow(request);
             return new BookShowResponseDto(booking.getId(), booking.getAmount(), ResponseStatus.SUCCESS, "SUCCESS");
         } catch (UserIsNotValid e) {
-            System.out.println("Not booked");
-            System.out.println("Added something else");
             return new BookShowResponseDto(null, 0, ResponseStatus.FAILURE, USER_INVALID_MESSAGE);
         } catch (ShowNotFound e) {
             return new BookShowResponseDto(null, 0, ResponseStatus.FAILURE, SHOW_INVALID_MESSAGE);
